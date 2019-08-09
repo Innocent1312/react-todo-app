29746c495bf45374fe7f486619d3cf1844e0a50d
Innocent1312 <anthonydev33@gmail.com>
2019-08-09 22:20:03 +0300

    Updates

---
+++ precache-manifest.00ae2661b4005b5ad17f4a430798740c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "addd1a7e3832507339390e853d6d8e70",
+    "url": "/react-todo-app/index.html"
+  },
+  {
+    "revision": "d8e031903362b9d12613",
+    "url": "/react-todo-app/static/css/main.09c00fd2.chunk.css"
+  },
+  {
+    "revision": "ba946bb66b8559c85e5f",
+    "url": "/react-todo-app/static/js/2.0975ab2f.chunk.js"
+  },
+  {
+    "revision": "d8e031903362b9d12613",
+    "url": "/react-todo-app/static/js/main.94572a0f.chunk.js"
+  },
+  {
+    "revision": "b1aeed9f6344163e5e40",
+    "url": "/react-todo-app/static/js/runtime~main.a3a648b2.js"
+  }
+]);
