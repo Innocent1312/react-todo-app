ba96b88f8fe8a16e7a87eb0ff379e1dcfe43c979
Innocent1312 <anthonydev33@gmail.com>
2019-09-09 10:38:12 +0300

    state for todo_list_item

--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -3,31 +3,59 @@ import './todo-list-item.css';
 
 
 export default class TodoListItem extends Component { // extends Component обов'язково для створення класів реакт // Можна зразу експортувати
-    onLabelClick = () => { // Створюю функцію СТРІЛКУ (ВАЖНО!!!) щоб виводить в консоль назву елемента списк
-        console.log(`Done: ${this.props.label}`) // Як ітог при нажатії на текст елемента списка в консоль виведеться те що написано в label елемента
+
+    state = { // State - оголошує внутрішній стан компонента в react. Його можна задати як в конструкторі так і в тілі класу
+        done: false,  // State задається об'єктом в який ми можемо передавати ключі (внутрішній стан компоненів)
+        important: false // Для important і done це false за замовчуванням
     };
 
-    render() {
-        const {label, important = false} = this.props; // Приймаю свойства об'єктів з app.js
+    onLabelClick = () => { // Створюю функцію СТРІЛКУ (ВАЖНО!!!) щоб відловлювати клік по назві todoшки
+        this.setState(({done})=>{//  функція з деструктурованим параметром done зі state відловлює останні зміни по компоненту
+            return{
+                done: !done // Код означає - поміняй стан (state) елемента (done) на протижежний
+            }
+        });
+    };
 
-        const style = { // Задаю стилі для important які будуть красити текст якщо important буде добавлено в елемент списку todo-list.js
-            color: important ? 'steelblue' : 'black',
-            fontWeight: important ? 'bold' : 'normal'
-        };
+    onMarkImportant = () =>{ // Створив функцію стрілку щоб відловлювати клік по кнопці "!"
+        this.setState(({important})=> { //  функція з деструктурованим параметром important зі state відловлює останні зміни по компоненту
+            return {
+                important: !important // Код означає - поміняй стан (state) елемента (important) на протижежний
+            }
+        });
+    };
+    render() {
+        const {label} = this.props; // Приймаю свойства об'єктів з app.js
+        const { done, important} = this.state; // Витягнули done із state деструктуризацією
+
+        let classNames = 'todo-list-item'; // Створили змінну з ім'ям класу (Юзаєм в 30 строчці)
+        if( done ){ // Якщо done в state = true
+            classNames += ' done'; // То добавляємо до classNames клас done який зачеркне елементи
+        }
+
+        if (important){
+            classNames += ' important';
+
+        }
+
+        // const style = { // Задаю стилі для important які будуть красити текст якщо important буде добавлено в елемент списку todo-list.js
+        //     color: important ? 'steelblue' : 'black',
+        //     fontWeight: important ? 'bold' : 'normal'
+        // };
 
         return (
-            <span className="todo-list-item"> {/*Спан для кожного елементу списку */}
+            <span className={classNames}> {/*Спан для кожного елементу списку */}
 
-                <span
+            <span
                     className="todo-list-item-label"
-                    style={style}
-                    onClick={this.onLabelClick}> {/*Елемент Списку*/} {/*Добавляю слухач нажатія на текст який верне функцію onLabelClick*/}
+                    onClick={this.onLabelClick} > {/*Елемент Списку*/} {/*Добавляю слухач нажатія на текст який верне функцію onLabelClick*/}
                     {label}
             </span>
 
           <button type="button"
-                  className="btn btn-outline-success btn-sm float-right">  {/*Кнопка з іконкою виділення*/}
-              <i className="fa fa-exclamation"/>
+                  className="btn btn-outline-success btn-sm float-right"
+                    onClick={this.onMarkImportant}>  {/*Кнопка з іконкою виділення*/} {/*Добавляю слухач що верне функцію onMarkImportant*/}
+                    <i className="fa fa-exclamation"/>
           </button>
 
           <button type="button"
